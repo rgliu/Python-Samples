c914d84b9b2b05564c377142ba87dfbb4e666316
Richard Liu <rliu@reflexionmedical.com>
2019-09-19 15:14:37 -0700

    Expanded on template

--- HTML Report Tool/auto_reporting.py
+++ HTML Report Tool/auto_reporting.py
@@ -9,11 +9,29 @@ env = Environment(
 	loader=FileSystemLoader(searchpath="template")
 )
 
-template = env.get_template("report.html")
+base_template = env.get_template("report.html")
+table_section_template = env.get_template("table_section.html")
+
+# Content
+title = "Test Report"
+sections = list()
+sections.append(table_section_template.render(
+    model="VGG19",
+    dataset="VGG19_results.csv",
+    table="Table goes here."
+))
+sections.append(table_section_template.render(
+    model="MobileNet",
+    dataset="MobileNet_results.csv",
+    table="Table goes here."
+))
 
 def main():
 	with open("output/report.html", "w") as f:
-		f.write(template.render(content=content))
+		f.write(base_template.render(            
+			title=title,
+            sections=sections
+		))
 		
 if __name__ == "__main__":
 	main()
